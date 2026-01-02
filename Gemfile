119e016349d5bb814e57afff27269fca7f57abfc
Sumit Yadav <yadavsumit18@gmail.com>
2026-01-02 22:52:58 +0530

    site update

--- Gemfile
+++
@@ -1,5 +0,0 @@
-source "https://rubygems.org"
-
-# This line is the magic key. It locks your local environment 
-# to match GitHub's server exactly.
-gem "github-pages", group: :jekyll_plugins
--- scripts/main.js
+++
@@ -1,7 +0,0 @@
-(function(){
-    // Reserved: central initializer if needed in future
-    SiteUtils.onReady(function(){
-        // allow manual trigger if needed
-        if(window.initTheme) window.initTheme();
-    });
-})();
